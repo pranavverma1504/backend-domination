9e36ef871e5c01f8d73b10ac26a3567ce0fc1da2
pranav verma <vermapranav001@gmail.com>
2026-01-28 03:20:51 +0530

    routing using nodejs

---
+++ Basics/routing using nodejs/index.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Home Page");
+  } else if (req.url === "/about") {
+    res.end("About Page");
+  } else if (req.url === "/contact") {
+    res.end("Contact Page");
+  } else {
+    res.end("404 Page Not Found");
+  }
+});
+
+server.listen(8000, () => {
+  console.log("Server running on http://localhost:8000");
+});
