88cfbe378e8a6edfc7b9af29ab5e6a7a997686a0
pranav verma <vermapranav001@gmail.com>
2026-02-02 21:22:08 +0530

    update operation in mongoDB using fondOneAndUpdate

---
+++ Basics/14.Update operation in mongoDB/findOneAndUpdate/index.js
@@ -0,0 +1,50 @@
+import express from "express";
+import connectDB from "./config/db.js";
+import User from "./models/User.js";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// UPDATE USER BY ID
+app.put("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { username, age } = req.body;
+
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      {
+        username,
+        age
+      },
+      {
+        new: true,        // updated document return krta h
+        runValidators: true
+      }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({
+        message: "User not found"
+      });
+    }
+
+    res.status(200).json({
+      message: "User updated successfully",
+      user: updatedUser
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Error updating user",
+      error: error.message
+    });
+  }
+});
+
+app.listen(PORT, async () => {
+  console.log(`Server running on port ${PORT}`);
+  await connectDB();
+});
